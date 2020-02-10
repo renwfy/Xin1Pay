773fb7de45a07fa4570bfe797816cc1d3c7d0829
LSD <renwfy@126.com>
2020-02-10 19:54:07 +0800

    x支付

--- src/js/pay.js
+++ src/js/pay.js
@@ -57,8 +57,8 @@ function urlDecode(String) {
 }
 
 function GetQueryString(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-    var r = window.location.search.substr(1).match(reg);
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    let r = window.location.search.substr(1).match(reg);
     if (r != null) return unescape(r[2]);
     return null;
 }
